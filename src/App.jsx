3f3d3b8a166e4116ad79832773ef3b55751d16aa
Artem <a.lipodat@mail.ru>
2022-06-15 12:20:33 +0400

    Старт проекта

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './style/App.css';
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <div className={'wrapper'}>
+        <Header/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
